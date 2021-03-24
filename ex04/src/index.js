f2137dec836587208ab24478090506fdebeb0432
hajdukka <nerminhajdo@gmail.com>
2021-03-24 06:39:47 +0100

    ex01-ex05

---
+++ ex04/src/index.js
@@ -0,0 +1,30 @@
+class Pets {
+  constructor(name, legs) {
+    this._name = name;
+    this._legs = legs;
+  }
+  walk() {
+    let numberOfLegs = this._name + " is walking on " + this._legs + " legs";
+    return numberOfLegs;
+  }
+}
+
+class Dog extends Pets {
+  constructor(name, legs) {
+    super(name);
+    this._legs = legs;
+  }
+  bark() {
+    let dogSay = this._name + " says WUF WUF";
+    return dogSay;
+  }
+}
+
+let dog = new Dog("Pujdo", 4);
+console.log(dog.walk());
+console.log(dog.bark());
+
+module.exports = {
+  Pets,
+  Dog,
+};
